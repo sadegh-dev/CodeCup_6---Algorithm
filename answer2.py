ff3e53559b5af144eb6c3161feb8d3d4a200ad13
sadegh <sadegh.rez@gmail.com>
2021-12-31 18:38:28 +0330

    Update answer2.py

--- answer2.py
+++ answer2.py
@@ -6,3 +6,17 @@ for x in range(t):
     d = input()
     b = d.split()
     arr.append(b)
+    y = int(x)
+    if x != y :
+        print("-1")
+    else:
+        qq = (n*x)
+        tt = s+(a*(n-1))
+        if qq != tt :
+            print("-1")
+        else:
+            result = x-a
+            if result == 0 :
+                print("-1")
+            else:
+                print(int(result))
