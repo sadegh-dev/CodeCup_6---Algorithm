1a0abd8b990c9d0a4f466e5842d0a5a07b7c91d9
sadegh <sadegh.rez@gmail.com>
2021-12-31 18:37:14 +0330

    Update answer1.py

--- answer1.py
+++ answer1.py
@@ -3,3 +3,15 @@ index = int(input())
 txt = "codecup6"
 arr = list(txt)
 
+if index < 9 :
+    index -= 1
+    result = arr[index]
+else :
+    a = divmod(index,8)
+    index = a[1]
+    index -= 1
+    result = arr[index]
+
+
+print(result)
+
